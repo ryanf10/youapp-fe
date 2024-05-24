40ad1a0c2755aeb915d413e757dc52dea3f111ed
ryanf10 <ryanfernaldy10@gmail.com>
2024-05-24 21:26:48 +0700

    chore: config pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icons/icon-192x192.png'></link>
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
